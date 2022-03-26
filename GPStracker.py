c9de3f1b99ae8ef1c1a62ace1b5c254691fbaa83
rdqst <renesis2014@hotmail.com>
2022-03-27 00:44:59 +0300

    v0.1

---
+++ GPStracker.py
@@ -0,0 +1,51 @@
+import eel
+import json
+
+eel.init('UI')
+
+points = []
+
+#Функция получения координат
+def Lizzard(points):
+    with open("geo.json", "r") as json_file:
+        geo = json.load(json_file)
+        
+        numberOfWorkers = geo['active_workers'] #получаем количество активных работников
+
+        points = []
+
+        workersCount = 1
+        #Получение массива координат
+        while workersCount <= numberOfWorkers: 
+            for txt in geo['id' + str(workersCount)]: #создали цикл, который будет работать построчно
+                points.append(txt['coordinateX'])
+                points.append(txt['coordinateY'])
+            #geo_str.append("-") #Добавление разделителя между координатами
+            workersCount += 1
+
+  
+    #Временный вывод
+
+    print(type(points))
+    print(points)
+    #/Временный вывод
+
+    return points
+
+#Функция получения информации о работниках
+
+
+   
+
+
+    
+        
+
+
+       
+        
+
+eel.call(Lizzard(points))
+
+
+eel.start("index.html", mode = 'chrome', size = (800, 960))
